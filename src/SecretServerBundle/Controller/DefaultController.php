35b15e21e55603640b2c1fc661a9a912cc918954
Boncsi <jabos.boncser@ecom-media.com>
2021-08-03 21:52:22 +0200

    Business logic move to secret service.

--- src/SecretServerBundle/Controller/DefaultController.php
+++ src/SecretServerBundle/Controller/DefaultController.php
@@ -14,77 +14,24 @@ class DefaultController extends Controller
     /**
      * @Route("/secret/list", name="getSecretList", methods={"GET","HEAD", "OPTIONS"})
      */
-    public function listAction()
+    public function listAction(SecretService $secretService)
     {
-        $secretRepository = $this->getDoctrine()->getManager()->getRepository("SecretServerBundle:Secret");
-
-        return $this->render('secret/list.html.twig', ['secretItems' => $secretRepository->getAllSecretItem()]);
+        return $this->render('secret/list.html.twig', ['secretItems' => $secretService->getListItems()]);
     }
 
     /**
      * @Route("/secret/{hash}", name="getSecretByHash", methods={"GET","HEAD", "OPTIONS"})
      */
-    public function getSecretAction($hash)
+    public function getSecretAction($hash, SecretService $secretService)
     {
-        $response         = new JsonResponse();
-        $secretRepository = $this->getDoctrine()->getManager()->getRepository("SecretServerBundle:Secret");
-
-        try {
-            /* @var $secretItem Secret */
-            $secretItem = $secretRepository->getSecretByHash($hash);
-
-            if (empty($secretItem)) {
-                throw new \Exception('Not be found!');
-            }
-
-            $secretService = new SecretService($secretItem);
-            $nowDateTime   = new \DateTime();
-
-            if ($secretItem->getCreatedAt() !== $secretService->getExpiresAtDateTime() && $secretService->getExpiresAtDateTime() < $nowDateTime) {
-                throw new \Exception('Expired - ExpiresAt!');
-            }
-
-            if ($secretItem->getRemainingViews() < 0) {
-                throw new \Exception('Expired - RemainingViews!');
-            }
-
-            /* @var $secretItem Secret */
-            $secretItem    = $secretRepository->reduceRemainingViewsCount($secretItem);
-            $secretService = new SecretService($secretItem);
-
-            $response->setData($secretService->getFilledData());
-        } catch (\Exception $e) {
-            $response->setStatusCode(404);
-        }
-
-        return $response;
+        return $secretService->getSecretByHash($hash);
     }
 
     /**
      * @Route("/secret", name="addNewSecret", methods={"POST","HEAD", "OPTIONS"})
      */
-    public function secretAction(Request $request)
+    public function secretAction(Request $request, SecretService $secretService)
     {
-        $response         = new JsonResponse();
-        $secretRepository = $this->getDoctrine()->getManager()->getRepository("SecretServerBundle:Secret");
-
-        try {
-            /* @var $secretItem Secret */
-            $secretItem = $secretRepository->addNew(
-                [
-                    'secret'         => filter_var($request->request->get('secret'), FILTER_SANITIZE_STRING),
-                    'expiresAt'      => filter_var($request->request->get('expireAfter'), FILTER_VALIDATE_INT),
-                    'remainingViews' => filter_var($request->request->get('expireAfterViews'), FILTER_VALIDATE_INT)
-                ]
-            );
-
-            $secretService = new SecretService($secretItem);
-
-            $response->setData($secretService->getFilledData());
-        } catch (\Exception $e) {
-            $response->setStatusCode(405);
-        }
-
-        return $response;
+        return $secretService->createNew($request);
     }
 }
--- src/SecretServerBundle/Service/SecretService.php
+++ src/SecretServerBundle/Service/SecretService.php
@@ -2,7 +2,11 @@
 
 namespace SecretServerBundle\Service;
 
+use SecretServerBundle\Repository\SecretRepository;
 use SecretServerBundle\Entity\Secret;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * SecretService
@@ -10,34 +14,98 @@ use SecretServerBundle\Entity\Secret;
 class SecretService
 {
     /**
-     * @var Secret
+     * Entity Manager
+     * @param EntityManagerInterface $_entityManager
      */
-    private $_secretEntity;
+    private $_entityManager;
+
+    /**
+     * @var SecretRepository
+     */
+    private $_secretRepository;
 
     /**
      * SecretService constructor.
-     * @param Secret $secretEntity
+     * @param EntityManagerInterface $entityManager
      */
-    public function __construct(Secret $secretEntity)
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $this->_secretEntity = $secretEntity;
+        $this->_entityManager    = $entityManager;
+        $this->_secretRepository = $this->_entityManager->getRepository("SecretServerBundle:Secret");
     }
 
-    public function getFilledData()
+    public function getListItems()
+    {
+        return $this->_secretRepository->getAllSecretItem();
+    }
+
+    public function createNew(Request $request)
+    {
+        $response = new JsonResponse();
+
+        try {
+            /* @var $secretItem Secret */
+            $secretItem = $this->_secretRepository->addNew(
+                [
+                    'secret'         => filter_var($request->request->get('secret'), FILTER_SANITIZE_STRING),
+                    'expiresAt'      => filter_var($request->request->get('expireAfter'), FILTER_VALIDATE_INT),
+                    'remainingViews' => filter_var($request->request->get('expireAfterViews'), FILTER_VALIDATE_INT)
+                ]
+            );
+
+            $response->setData($this->getFilledData($secretItem));
+        } catch (\Exception $e) {
+            $response->setStatusCode(405);
+        }
+
+        return $response;
+    }
+
+    public function getSecretByHash($hash)
+    {
+        $response = new JsonResponse();
+
+        try {
+            /* @var $secretItem Secret */
+            $secretItem = $this->_secretRepository->getSecretByHash($hash);
+
+            if (empty($secretItem)) {
+                throw new \Exception('Not be found!');
+            }
+
+            $nowDateTime = new \DateTime();
+
+            if ($secretItem->getCreatedAt() !== $this->getExpiresAtDateTime($secretItem) && $this->getExpiresAtDateTime($secretItem) < $nowDateTime) {
+                throw new \Exception('Expired - ExpiresAt!');
+            }
+
+            if ($secretItem->getRemainingViews() < 0) {
+                throw new \Exception('Expired - RemainingViews!');
+            }
+
+            $response->setData($this->getFilledData($this->_secretRepository->reduceRemainingViewsCount($secretItem)));
+        } catch (\Exception $e) {
+            $response->setStatusCode(404);
+        }
+
+        return $response;
+    }
+
+    public function getExpiresAtDateTime(Secret $secretItem)
+    {
+        $expiresAt = new \DateTime($secretItem->getCreatedAt()->format('Y-m-d H:i:s'));
+
+        return $expiresAt->modify("+{$secretItem->getExpiresAt()} minutes");
+    }
+
+    public function getFilledData(Secret $secretItem)
     {
         return [
-            'hash'           => $this->_secretEntity->getHash(),
-            'secretText'     => $this->_secretEntity->getSecret(),
-            'createdAt'      => $this->_secretEntity->getCreatedAt()->format('Y-m-d\TH-i-s.\0\0\0\Z'),
-            'expiresAt'      => $this->getExpiresAtDateTime()->format('Y-m-d\TH-i-s.\0\0\0\Z'),
-            'remainingViews' => $this->_secretEntity->getRemainingViews()
+            'hash'           => $secretItem->getHash(),
+            'secretText'     => $secretItem->getSecret(),
+            'createdAt'      => $secretItem->getCreatedAt()->format('Y-m-d\TH-i-s.\0\0\0\Z'),
+            'expiresAt'      => $this->getExpiresAtDateTime($secretItem)->format('Y-m-d\TH-i-s.\0\0\0\Z'),
+            'remainingViews' => $secretItem->getRemainingViews()
         ];
-    }
-
-    public function getExpiresAtDateTime()
-    {
-        $expiresAt = new \DateTime($this->_secretEntity->getCreatedAt()->format('Y-m-d H:i:s'));
-
-        return $expiresAt->modify("+{$this->_secretEntity->getExpiresAt()} minutes");
     }
 }
