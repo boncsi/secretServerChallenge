34213d40480d726b2168f7b3e05a517b862bec0d
Boncsi <jabos.boncser@ecom-media.com>
2021-08-05 23:40:38 +0200

    Create first test in project.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -13,9 +14,10 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/list.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $response = new JsonResponse();
+
+        $response->setData(['data' => 'Hello, this is the Secret Server challenge!']);
+
+        return $response;
     }
 }
--- tests/SecretServerBundle/Controller/DefaultControllerTest.php
+++ tests/SecretServerBundle/Controller/DefaultControllerTest.php
@@ -3,15 +3,45 @@
 namespace SecretServerBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use SecretServerBundle\Entity\Secret;
+use SecretServerBundle\Service\SecretService;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        static::bootKernel();
 
-        $crawler = $client->request('GET', '/');
+        $entityManager = static::$kernel->getContainer()->get('doctrine.orm.entity_manager');
+        $secretItem    = new Secret();
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $secretItem->setRemainingViews(10);
+        $secretItem->setHash('asddewerfsdfewr2342bfdfgb46');
+        $secretItem->setExpiresAt(200);
+        $secretItem->setCreatedAt(new \DateTime());
+        $secretItem->setSecret('Secret string');
+
+        $secretRepository = $this->createMock(ObjectRepository::class);
+        $secretRepository->expects($this->any())
+            ->method('find')
+            ->willReturn($secretItem);
+
+        $objectManager = $this->createMock(ObjectManager::class);
+        $objectManager->expects($this->any())
+            ->method('getRepository')
+            ->willReturn($secretRepository);
+
+        $secretService = new SecretService($entityManager);
+        $result        = $secretService->getFilledData($secretItem);
+
+        $this->assertIsArray($result);
+        $this->assertNotEmpty($result['hash']);
+        $this->assertNotEmpty($result['secretText']);
+        $this->assertNotEmpty($result['createdAt']);
+        $this->assertNotEmpty($result['expiresAt']);
+        $this->assertNotEmpty($result['remainingViews']);
     }
 }
