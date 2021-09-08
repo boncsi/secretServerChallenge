045649f50dbf3bcf9fbc4947ede32c7f83bcfa71
Boncsi <jabos.boncser@ecom-media.com>
2021-09-08 12:33:52 +0200

    vue

--- client/secret/src/App.vue
+++ client/secret/src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
+  <div id="app" class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" id="menuLabel" href="/">Secret Client Home page</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
+
     <router-view />
   </div>
 </template>
--- client/secret/src/views/Home.vue
+++ client/secret/src/views/Home.vue
@@ -1,44 +1,56 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <p>{{title}}</p>
+  <div class="container mt-4">
+    <div class="row">
+      <h1>Secret List</h1>
+
+      <hr />
+
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Hash</th>
+            <th scope="col">Secret</th>
+            <th scope="col">Remaining views</th>
+            <th scope="col">Created at</th>
+            <th scope="col">Expires at</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(secret, i) in secrets" :key="i">
+            <td>{{ secret.hash }}</td>
+            <td>{{ secret.secret }}</td>
+            <td>{{ secret.remainingViews }}</td>
+            <td>{{ secret.createdAt }}</td>
+            <td>{{ secret.expiresAt }}</td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
   </div>
 </template>
 
 <script>
-  import axios from 'vue-axios';
+  import axios from 'axios';
 
   export default {
-    //Ha kívülről akarom elérni akkor itt tudom nevesíteni az exportal egybekötve tudom elérhetővé tenni kívülről.
-    name: 'home',
+    name: 'secretList',
 
-    //Compneenseket itt tudok felvenni
-    comments: {
-
-    },
-
-    //változók, amiket használhatok a templetemben
-    //statikus adatok
     data() {
       return {
-        title: 'Boncsi Home'
+        secrets: []
       }
     },
 
-    //függvények, metódusok amik már figyelik a layouton a változáspokat, eseményeket
-    //dinamikus adatok
-    computed: {
-
-    },
-
-    //Hook a komponens létrehozásakor futtatható dolgok jönnek ide
     created() {
-
-    },
-
-    //Definiciók amiket a komponensünk akar használni
-    methods: {
-
+      axios.get('https://firstsymfonyproject.localhost/api/secret/list', {
+        auth: {
+          username: 'boncsi',
+          password: 'Almafa123!'
+        }
+      })
+      .then(response => this.secrets = response.data)
+      .catch(err => console.log(err))
     }
   }
 </script>
