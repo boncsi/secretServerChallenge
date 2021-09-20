da7183a82ea8c4706b9fe1fb56d0dda5a60fa642
Boncsi <bonjan11@gmail.com>
2021-09-20 13:56:19 +0200

    SEC-14 - List item refact

---
+++ client/secret/src/components/secret/Item.vue
@@ -0,0 +1,31 @@
+<template>
+    <tr>  
+        <td>{{ secret.hash }}</td>
+        <td>{{ secret.secret }}</td>
+        <td>{{ secret.remainingViews }}</td>
+        <td>{{ secret.createdAt }}</td>
+        <td>{{ secret.expiresAt }}</td>
+    </tr>
+</template>
+
+<script>
+  export default {
+    name: 'secretItem',
+
+    props: {
+        secret: {
+            type: Object,
+            default: function() {
+                return { 
+                    hash: 0,
+                    secret: 0,
+                    remainingViews: 0,
+                    createdAt: 0,
+                    expiresAt: 0
+                };
+            },
+            required: true
+        }
+    }
+  }
+</script>
